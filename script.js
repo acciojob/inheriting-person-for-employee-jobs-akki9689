c4d187b1644d4c43b6d6049e1f0a08d59992476f
Akash Puri <104100720+akki9689@users.noreply.github.com>
2023-10-23 05:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ Person.prototype.greet=function() {
 
 function Employee(name, age, jobTitle) {
 	Person.call(this, name, age);
-    this.jobTitle = jobTitle;
+    
 }
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee; // Reset the constructor to Employee
 
 Employee.prototype.jobGreet = function() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 const person1 = new Person("Alice", 25);
 person1.greet();
 
-const employee1 = new Employee("Bob", 30, "Developer");
+
 employee1.greet();
 employee1.jobGreet();
 // Do not change code below this line
